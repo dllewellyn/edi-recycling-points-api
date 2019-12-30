2f3637ebcfba0a55f3117a1d979431ded169a4c8
Daniel Llewellyn <daniel.llewellyn@xdesign.com>
2019-12-30 17:22:07 +0000

    scripts

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+aws cloudformation deploy --template-file /Users/danielllewellyn/Developer/Personal/enviroment/edi-recycling-points-api/output-sam.yaml --stack-name edi-recycling --capabilities CAPABILITY_IAM
